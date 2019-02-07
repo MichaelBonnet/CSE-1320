d286aea8df5fbf6a180196e446504bb44902c119
Michael Bonnet <36342359+MichaelBonnet@users.noreply.github.com>
2019-02-07 01:00:08 -0600

    Add files via upload

--- hw03/hw03_bonnet_michael.c
+++ hw03/hw03_bonnet_michael.c
@@ -1,1 +1,25 @@
+#include<stdio.h>
 
+char calculator[47][5]
+
+
+int main(void)
+{
+	int input;
+	
+	do
+	{
+		scanf("%d", input);
+		if (input == -1)
+		{
+			return 0;
+		}
+			else
+			{
+				// do things
+			}
+			
+	} while (input != -1);
+
+	return 0;
+}
