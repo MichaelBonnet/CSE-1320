688f8fc488d85d188e9f6ca5191781d4aff2058f
Michael Bonnet <36342359+MichaelBonnet@users.noreply.github.com>
2019-02-12 16:47:04 -0600

    Update hw03_bonnet_michael.c

--- hw03/hw03_bonnet_michael.c
+++ hw03/hw03_bonnet_michael.c
@@ -1,9 +1,74 @@
 #include<stdio.h>
 
 
-void calculator_view(int input)
+void line1(int input)
 {
-	char calculator[5][6];
+	if (input == 0 | 8 | 9 )     { printf("%c%c%c%c%c%c", ' ', '-', '-', '-', ' ', ' ') }
+	if (input == 2 | 3 | 5 | 7 ) { printf("%c%c%c%c%c%c", '-', '-', '-', '-', ' ', ' ') }
+	if (input == 1 | 4 | 6 )     { printf("%c%c%c%c%c%c", ' ', ' ', ' ', ' ', ' ', ' ') }
+}
+
+void line2()
+{
+	if (input == 0 | 8 | 9 )     { printf("%c%c%c%c%c%c", '|', ' ', ' ', ' ', '|', ' ') }
+	if (input == 1 | 3 )         { printf("%c%c%c%c%c%c", ' ', ' ', ' ', ' ', '|', ' ') }
+	if (input == 2 | 7 )         { printf("%c%c%c%c%c%c", ' ', ' ', ' ', '/', ' ', ' ') }
+	if (input == 4 | 5 | 6 )     { printf("%c%c%c%c%c%c", '|', ' ', ' ', ' ', ' ', ' ') } 
+}
+
+void line3()
+{
+	if (input == 0 )             { printf("%c%c%c%c%c%c", '|', ' ', ' ', ' ', '|', ' ') }
+	if (input == 1 )             { printf("%c%c%c%c%c%c", ' ', ' ', ' ', ' ', '|', ' ') }
+	if (input == 2 | 7 )         { printf("%c%c%c%c%c%c", ' ', ' ', '/', ' ', ' ', ' ') }
+	if (input == 3 | 4 | 5 )     { printf("%c%c%c%c%c%c", '-', '-', '-', '-', ' ', ' ') }
+	if (input == 6 | 8 | 9 )     { printf("%c%c%c%c%c%c", ' ', '-', '-', '-', ' ', ' ') }
+}
+
+void line4()
+{
+	if (input == 0 | 8 | 6)      { printf("%c%c%c%c%c%c", '|', ' ', ' ', ' ', '|', ' ') }
+	if (input == 1 | 3 | 5 | 9 ) { printf("%c%c%c%c%c%c", ' ', ' ', ' ', ' ', '-', ' ') }
+	if (input == 2 | 7 )         { printf("%c%c%c%c%c%c", ' ', '/', ' ', ' ', ' ', ' ') }
+	if (input == 4 )             { printf("%c%c%c%c%c%c", ' ', ' ', '^', ' ', ' ', ' ') }
+
+}
+
+void line5()
+{
+	if (input == 0 | 6 | 8 )     { printf("%c%c%c%c%c%c", ' ', '-', '-', '-', ' ', ' ') }
+	if (input == 1 | 7 | 9 )     { printf("%c%c%c%c%c%c", ' ', ' ', ' ', ' ', ' ', ' ') }
+	if (input == 2 | 3 | 5 )     { printf("%c%c%c%c%c%c", '-', '-', '-', '-', ' ', ' ') }
+	if (input == 4 )             { printf("%c%c%c%c%c%c", ' ', ' ', '^', ' ', ' ', ' ') }
+}
+
+void calculator(int array[])
+{
+	for (int i = 7;, i > -1; i--)
+	{
+		line1(array[i]);
+	}
+	printf("\n");
+	for (int i = 7;, i > -1; i--)
+	{
+		line2(array[i]);
+	}
+	printf("\n");
+	for (int i = 7;, i > -1; i--)
+	{
+		line3(array[i])
+	}
+	printf("\n");
+	for (int i = 7;, i > -1; i--)
+	{
+		line4(array[i])
+	}
+	printf("\n");
+	for (int i = 7;, i > -1; i--)
+	{
+		line5(array[i])
+	}
+	printf("\n");
 }
 
 
@@ -21,7 +86,7 @@ int main(void)
 	int digit_storage[8]; // this array stores the terms created by the do-while loop for later use
 	
 	do
-    {
+    	{
         printf("Enter integer value: "); // prompts user for input
         scanf("%d", &input); // reads user input from stdin
         if (input == -1)
@@ -40,6 +105,6 @@ int main(void)
     			printf("%d\n", digit_storage[i]);
     		}
         }
-    } while (input != -1); // keeps prompting for input until the user types -1 to exit
+    	} while (input != -1); // keeps prompting for input until the user types -1 to exit
     return 0;
 }
