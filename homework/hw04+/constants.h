8da4d5dc118bdb0ca98d853286ee91f243b6ba3d
MichaelBonnet <maabonnet@gmail.com>
2019-03-26 16:50:58 -0500

    gettin hw5 all up in there

---
+++ homework/hw04+/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Macros and Constant Definitions
+#define MAX_WORDS 400000  // max number of words that can be entered
+#define MAX_CHARS 1024    // max length of a word that can be entered
+
+#endif
--- homework/hw05/readme.txt
+++
@@ -1,1 +0,0 @@
-assignment not yet received
